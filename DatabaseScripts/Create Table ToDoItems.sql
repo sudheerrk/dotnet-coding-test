617c8e296044e43f437ca5569c127e7d34e2e7f3
sales-i <development@sales-i.com>
2016-05-12 16:40:55 +0100

    database table create script

--- DatabaseScripts/Create Table ToDoItems.sql
+++ DatabaseScripts/Create Table ToDoItems.sql
@@ -1,11 +1,12 @@
 CREATE TABLE [dbo].[ToDoItems](
-[id] [uniqueidentifier] NOT NULL,
-[title] [nvarchar](250) NOT NULL,
-[description] [nvarchar](max) NULL,
-[complete] [bit] NOT NULL,
+	[id] [uniqueidentifier] NOT NULL,
+	[title] [nvarchar](250) NOT NULL,
+	[description] [nvarchar](max) NULL,
+	[complete] [bit] NOT NULL,
+	[relatedId] [uniqueidentifier] NULL,
  CONSTRAINT [PK_ToDoItems] PRIMARY KEY CLUSTERED 
 (
-[id] ASC
+	[id] ASC
 )WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
@@ -13,3 +14,5 @@ GO
 
 ALTER TABLE [dbo].[ToDoItems] ADD  CONSTRAINT [DF_ToDoItems_complete]  DEFAULT ((0)) FOR [complete]
 GO
+
+
